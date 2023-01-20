9de88659ff9b7636eefdc2dd54f6051edfce5dfb
Tamzid Israk <tamzidisrak12@gmail.com>
2023-01-20 13:06:15 +0600

    Quotes android app

---
+++ app/src/main/java/com/example/quotes/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quotes.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
